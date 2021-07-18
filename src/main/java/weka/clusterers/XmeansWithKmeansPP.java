7ea8aec1aafc8f443f09b47852e732816aa80fd4
pawel.trajdos@pwr.edu.pl <pawel.trajdos@pwr.edu.pl>
2021-07-18 17:22:45 +0200

    1. XmeansWithKmeansPP -- K-Means++ initialization has been added. + Tests

--- src/main/java/weka/clusterers/XmeansWithKmeansPP.java
+++ src/main/java/weka/clusterers/XmeansWithKmeansPP.java
@@ -3,6 +3,18 @@
  */
 package weka.clusterers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.math3.util.Pair;
+
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
+
 /**
  * Class implements Xmeans with the initialization using K-Means++
  * @author pawel trajdos
@@ -21,7 +33,94 @@ public class XmeansWithKmeansPP extends XMeans {
 	 * 
 	 */
 	public XmeansWithKmeansPP() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
+	
+	@Override
+	protected Instances makeCentersRandomly(Random random0, Instances model, int numClusters) {
+		//return super.makeCentersRandomly(random0, model, numClusters);
+		
+		Instances clusterCenters = new Instances(model, numClusters);
+	    this.m_NumClusters = numClusters;
+	    
+	    int instIndex = Math.abs(random0.nextInt()) % this.m_Instances.numInstances();
+	    Instance lastSelected = this.m_Instances.get(instIndex);
+	    clusterCenters.add(lastSelected);
+	    
+	    if(this.m_NumClusters==1)
+	    	return clusterCenters;
+	    
+	    Instance tmpInstance = null;
+	    for(int i=1;i<this.m_NumClusters;i++) {
+	    	tmpInstance = this.selectNextCenter(lastSelected, random0);
+	    	clusterCenters.add(tmpInstance);
+	    	lastSelected = tmpInstance;
+	    }
+	    
+		
+		return clusterCenters;
+		
+	}
+	
+	protected List<Pair<Instance,Double>> getSquaredDistances(Instance srcInst){
+		int numInst  = this.m_Instances.numInstances();
+		double dist=0;
+		double[] dists = new double[numInst];
+		double sum=0;
+		for(int i=0;i<numInst;i++) {
+			dist = this.m_DistanceF.distance(this.m_Instances.get(i), srcInst);
+			dist*=dist;
+			dists[i]=dist;
+			sum+=dist;
+		}
+		if(sum>0)
+			Utils.normalize(dists);
+		
+		List<Pair<Instance,Double>> distList = new ArrayList<Pair<Instance, Double>>(numInst);
+		for(int i=0;i<dists.length;i++) {
+			distList.add(new Pair<Instance, Double>(this.m_Instances.get(i), dists[i]));
+		}
+		
+		Collections.sort(distList, new Comparator<Pair<Instance,Double>>(){
+
+			@Override
+			public int compare(Pair<Instance, Double> o1, Pair<Instance, Double> o2) {
+				double val1 = o1.getValue();
+				double val2 = o2.getValue();
+				
+				if(val1<val2)
+					return 1;
+				if(val1>val2)
+					return -1;
+				
+				return 0;
+			}
+			
+		});
+		return distList;
+	}
+	
+	protected Instance selectNextCenter(Instance lastCenter, Random random) {
+		List<Pair<Instance,Double>> sqList = this.getSquaredDistances(lastCenter);
+		Instance selectedInstance = this.rouletteSelector(sqList, random);
+		return selectedInstance;
+	}
+	
+	protected Instance rouletteSelector(List<Pair<Instance,Double>> squaredDistList,Random random) {
+		Instance selectedInstance=null;
+		
+		double rndVal = random.nextDouble();
+		double sum=0;
+		int cnt=0;
+		int listLen = squaredDistList.size();
+		while(sum<rndVal && cnt<listLen) {
+			selectedInstance = squaredDistList.get(cnt).getKey();
+			sum += squaredDistList.get(cnt).getValue();
+			cnt++;
+		}	
+		return selectedInstance;
+	}
+	
+	
 
 }
--- src/test/java/weka/clusterers/XmeansWithKmeansPPTest.java
+++ src/test/java/weka/clusterers/XmeansWithKmeansPPTest.java
@@ -1,7 +1,10 @@
 package weka.clusterers;
 
-import weka.core.Capabilities;
-import weka.core.Capabilities.Capability;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.math3.util.Pair;
+
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.tools.data.RandomDataGenerator;
@@ -59,6 +62,79 @@ public class XmeansWithKmeansPPTest extends AbstractClustererTest {
 			fail("An exception has been caught " + e.getMessage());
 		}
 	 }
+	
+	public void testDistanceList() {
+		 XmeansWithKmeansPP clusterer = (XmeansWithKmeansPP) this.getClusterer();
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(0);
+		 gen.setAddClassAttrib(false);
+		 
+		 Instances data = gen.generateData();
+		 Instance tmpInstance = data.get(0);
+		 
+		 try {
+			clusterer.buildClusterer(data);
+		} catch (Exception e) {
+			fail("An exception has been thrown!" + e.getMessage());
+		}
+		 
+		 List<Pair<Instance,Double>> sqDistList =clusterer.getSquaredDistances(tmpInstance);
+		 
+		 assertTrue("List not null", sqDistList !=null);
+		 
+		 int listLen = sqDistList.size();
+		 boolean increasingOrder=true;
+		 
+		 for(int i=0 ;i<listLen-1;i++) {
+			 if(sqDistList.get(i).getValue() < sqDistList.get(i+1).getValue()) {
+				 increasingOrder=false;
+				 break;
+			 }	 
+		 }
+		 
+		 assertTrue("Decreasing order", increasingOrder);
+		 
+	}
+	
+	public void testRouleteSelector() {
+		XmeansWithKmeansPP clusterer = (XmeansWithKmeansPP) this.getClusterer();
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(0);
+		 gen.setAddClassAttrib(false);
+		 
+		 Instances data = gen.generateData();
+		 Instance tmpInstance = data.get(0);
+		 
+		 Instance selectedInstance = null;
+		 Random rnd = new Random(0);
+		 try {
+			clusterer.buildClusterer(data);
+			List<Pair<Instance,Double>> sqDistList =clusterer.getSquaredDistances(tmpInstance);
+			selectedInstance = clusterer.rouletteSelector(sqDistList, rnd);
+			
+			assertTrue("Selected not null", selectedInstance!=null);
+			
+		} catch (Exception e) {
+			fail("An exception has been thrown!" + e.getMessage());
+		}
+	}
+	
+	public void testDateAttribs() {
+		XmeansWithKmeansPP clusterer = (XmeansWithKmeansPP) this.getClusterer();
+		 RandomDataGenerator gen = new RandomDataGenerator();
+		 gen.setNumNominalAttributes(0);
+		 gen.setNumNumericAttributes(0);
+		 gen.setNumDateAttributes(4);
+		 gen.setAddClassAttrib(false);
+		 
+		 Instances data = gen.generateData();
+		 
+		 try {
+			clusterer.buildClusterer(data);
+		} catch (Exception e) {
+			fail("An exception has been caught:" + e.getMessage());
+		}
+	}
 
 
 }
